43009d0a0a29a8335f4644aac55b071a765266b9
webstergrill <rwebster90@me.com>
2017-03-12 11:39:36 -0500

    Update estimote-telemetry.js

--- estimote-telemetry.js
+++ estimote-telemetry.js
@@ -271,4 +271,71 @@ noble.on('discover', function(peripheral) {
 
   var telemetryPacket = parseEstimoteTelemetryPacket(data);
   if (telemetryPacket) { console.log(telemetryPacket); }
+ 
+
+var util = require('util');
+var logger = require('../../util/logger');
+
+function kinesis-producer(kinesis, config) {
+  var log = logger().getLogger('kinesis-producer');
+
+  function _createStreamIfNotCreated(callback) {
+    var params = {
+      ShardCount : config.shards,
+      StreamName : config.stream
+    };
+
+    kinesis.createStream(params, function(err, data) {
+      if (err) {
+        if (err.code !== 'ResourceInUseException') {
+          callback(err);
+          return;
+        }
+        else {
+          log.info(util.format('%s stream is already created. Re-using it.', config.stream));
+        }
+      }
+      else {
+        log.info(util.format("%s stream doesn't exist. Created a new stream with that name ..", config.stream));
+      }
+
+      // Poll to make sure stream is in ACTIVE state before start pushing data.
+      _waitForStreamToBecomeActive(callback);
+    });
+  }
+
+  function _waitForStreamToBecomeActive(callback) {
+    kinesis.describeStream({StreamName : config.stream}, function(err, data) {
+      if (!err) {
+        log.info(util.format('Current status of the stream is %s.', data.StreamDescription.StreamStatus));
+        if (data.StreamDescription.StreamStatus === 'ACTIVE') {
+          callback(null);
+        }
+        else {
+          setTimeout(function() {
+            _waitForStreamToBecomeActive(callback);
+          }, 1000 * config.waitBetweenDescribeCallsInSeconds);
+        }
+      }
+    });
+  }
+
+  function _writeToKinesis() {
+    var recordParams = {
+      Data : telemetryPacket,
+      PartitionKey : shortIdentifier,
+      StreamName : config.stream
+    };
+
+    kinesis.putRecord(recordParams, function(err, data) {
+      if (err) {
+        log.error(err);
+      }
+      else {
+        log.info('Successfully sent data to Kinesis.');
+      }
+    });
+  }
+module.exports = kinesis-producer;
+
 });
