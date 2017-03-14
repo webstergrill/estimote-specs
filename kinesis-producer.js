eab24a70541e86620c36c87e930b583df5b33574
webstergrill <rwebster90@me.com>
2017-03-14 12:44:44 -0500

    Update kinesis-producer.js

--- kinesis-producer.js
+++ kinesis-producer.js
@@ -2,7 +2,7 @@
 
 var AWS = require('aws-sdk');
 var config = require('./config');
-var producer = require('./telemetry-producer');
+var producer = require('./estimote-telemetry-producer');
 
 var kinesis = new AWS.Kinesis({region : config.kinesis.region});
-producer(kinesis, config.telemetry-producer).run();
+producer(kinesis, config.producer).run();
